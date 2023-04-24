d78e37fda6d8077471f7288514d6a77d6eb14581
ivan2374 <ivanmartines@hotmail.com>
2023-04-23 21:34:01 -0300

    Habia quedado un numeroRandom sin modificar

--- reseñas.js
+++ reseñas.js
@@ -1,8 +1,8 @@
-//Funcion que genera un numero aleatorio que se usa como indice para obtener los datos de la api rest.
+
 let numeroRandom = (min, max) =>
     Math.floor(Math.random() * (max - min)) + min;
 
-// Funcion que realiza una peticion GET a una url dada como parametro y actualiza los elementos recibidos, todo esto lo realiza de forma asincronica sin bloquear la pagina mientras se espera la respuesta de la misma.
+
 function getReview(url, reviewSelector, authorSelector, photoSelector) {
     fetch(url)
         .then(response => response.json())
@@ -19,20 +19,20 @@ function getReview(url, reviewSelector, authorSelector, photoSelector) {
         });
 }
 
-// Url para las peticiones ajax agregando la funcion numeroRandom para generar aleatoriamente las distintas reseñas.
+
 let reviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 61)}.json`;
 let firstReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 61)}.json`;
 let secondReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 61)}.json`;
-let thirdReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 32)}.json`;
+let thirdReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 61)}.json`;
 
 
-// Se utiliza la funcion getReview para acutalizar los nodos 
+
 getReview(reviewUrl, '.review', '.author', '.photo');
 getReview(firstReviewUrl, '.first-review', '.first-author', '.first-photo');
 getReview(secondReviewUrl, '.second-review', '.second-author', '.second-photo');
 getReview(thirdReviewUrl, '.third-review', '.third-author', '.third-photo');
 
-//Funcion encargada de actualizar los datos una vez que se llega al ultimo slide.
+
 function updateReview() {
     let reviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 61)}.json`;
     let firstReviewUrl = `https://powergym-adm-default-rtdb.firebaseio.com/${numeroRandom(0, 61)}.json`;
@@ -51,7 +51,7 @@ let contador = 1
 let tamanoWidth = sliderIndividual[0].clientWidth;
 let intervalo = 9000;
 
-//Funcion que se encarga de tomar nuevamente el Width del Slider a la hora de realizar un resize a la pagina, con esto el slider queda responsive
+
 window.addEventListener("resize", function () {
     tamanoWidth = sliderIndividual[0].clientWidth;
 })
@@ -60,7 +60,7 @@ setInterval(function tiempo() {
     slices();
 }, intervalo);
 
-// La funcion Slices se encarga de proporcionarle un traslado en X hacia la derecha, esto es gracias al operador "-".
+
 function slices() {
     slider.style.transform = 'translate(' + (- tamanoWidth * contador) + 'px)';
     slider.style.transition = 'transform 1s';
